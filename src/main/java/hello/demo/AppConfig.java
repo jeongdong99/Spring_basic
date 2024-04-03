629e973acb0a81226507b7e353552336e2cd2095
jeongdong99 <sjd0219@gmail.com>
2024-04-03 18:02:03 +0900

    :art: Refactoring AppConfig

---
+++ src/main/java/hello/demo/AppConfig.java
@@ -0,0 +1,29 @@
+package hello.demo;
+
+import hello.demo.discount.DiscountPolicy;
+import hello.demo.discount.FixDiscountPolicy;
+import hello.demo.member.MemberRepository;
+import hello.demo.member.MemberService;
+import hello.demo.member.MemberServiceImpl;
+import hello.demo.member.MemoryMemberRepository;
+import hello.demo.order.OrderService;
+import hello.demo.order.OrderServiceImpl;
+
+public class AppConfig {
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
+}
--- src/main/java/hello/demo/OrderApp.java
+++ src/main/java/hello/demo/OrderApp.java
@@ -1,5 +1,7 @@
 package hello.demo;
 
+import hello.demo.member.Grade;
+import hello.demo.member.Member;
 import hello.demo.member.MemberService;
 import hello.demo.member.MemberServiceImpl;
 import hello.demo.order.Order;
@@ -8,9 +10,16 @@ import hello.demo.order.OrderServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
 
+        Order order = orderService.createOrder(memberId,"itemA",10000);
+
+        System.out.println("order =" + order);
     }
 }
